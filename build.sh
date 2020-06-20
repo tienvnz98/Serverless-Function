5c511ef2a0f18bfab6f420f867b04aca0427fee9
Minh Tien <tienvnz98@gmail.com>
2020-06-20 18:14:40 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-docker build -t 'tiennm/serverless-function-node:0.1' . 
+VERSION=$(cat package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[[:space:]]')
+IMAGE_NAME="serverless-function"
+
+# Building
+echo 'Building '${IMAGE_NAME}' version '$VERSION'...'
+docker build -t $DOCKER_HUB_ACCOUNT/$IMAGE_NAME:$VERSION .
+
+# Deploy
+echo 'Deploying '$DOCKER_HUB_ACCOUNT/$IMAGE_NAME' version '$VERSION'...'
+docker login --username=$DOCKER_HUB_ACCOUNT --password=$DOCKER_HUB_PASSWORD
+docker push $DOCKER_HUB_ACCOUNT/$IMAGE_NAME:$VERSION
