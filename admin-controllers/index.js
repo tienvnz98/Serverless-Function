e60ab8aa7f31a5fc9098e2c60133192da7ff8b14
Minh Tien <tienvnz98@gmail.com>
2020-06-20 17:51:52 +0700

    update auth

--- admin-controllers/index.js
+++ admin-controllers/index.js
@@ -7,13 +7,10 @@ const createFunction = require('./create-function');
 const updateFunction = require('./update-function');
 const readFunction = require('./read-function');
 const deleteFunction = require('./delete-function');
-const adminLogin = require('./admin-login');
 
 router.get('/admin/home', async (ctx) => {
     ctx.body = 'Serverless admin API runing.';
 });
-
-router.post('/admin/login', adminLogin);
 router.get('/admin/function/tree', readFunction.tree);
 router.get('/admin/function/read', readFunction.read);
 router.delete('/admin/function/delete', deleteFunction);
