e38ab90a659180db3cc76cad003855abec03fe39
Minh Tien <tienvnz98@gmail.com>
2020-06-20 22:41:16 +0700

    update custom middleware

--- core/app.js
+++ core/app.js
@@ -5,22 +5,21 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
 const app = new Koa();
+const customMiddle = require('./load-middle');
 const router = require('./router');
-const middleFunction = require('./middlewares/middle-function');
-const errorHandler = require('./middlewares/error-handler');
 const apiPort = process.env.API_PORT || 4200;
 const webSocket = require('./websocket');
 
 (async () => {
   await webSocket();
-  
-  app
-    .use(cors())
-    .use(bodyParser())
-    .use(middleFunction())
-    .use(errorHandler())
-    .use(router.routes());
+  app.use(cors());
+  app.use(bodyParser());
 
+  for (const func of customMiddle) {
+    app.use(func.handers);
+  }
+  
+  app.use(router.routes());
   app.listen(apiPort, () => {
     console.log(`API runing on port ${apiPort}. Process ID: ${process.pid}`);
   });
--- core/middlewares/error-handler.js
+++ core/middlewares/error-handler.js
@@ -1,12 +1,12 @@
 'use strict';
 
 
-module.exports = () => {
-  return async (ctx, next) => {
-    try {
-      await next();
-    } catch (err) {
-      return ctx.showError(ctx, err.message, err.status || 500);
-    }
+module.exports.handers = async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    return ctx.showError(ctx, err.message, err.status || 500);
   }
 }
+
+module.exports.order = 2;
