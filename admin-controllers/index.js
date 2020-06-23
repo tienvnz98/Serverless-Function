8909d38c55ba6f4a3278e937d7c003b1d25a47bb
Minh Tien <tienvnz98@gmail.com>
2020-06-23 19:51:00 +0700

    update

--- admin-controllers/index.js
+++ admin-controllers/index.js
@@ -7,15 +7,20 @@ const createFunction = require('./functions/create');
 const updateFunction = require('./functions/update');
 const readFunction = require('./functions/read');
 const deleteFunction = require('./functions/delete');
+const { localIpAdress } = require('./get-current-ip');
+
 
 const createMiddleware = require('./middlewares/create');
 const updateMiddleware = require('./middlewares/update');
 const readMiddleware = require('./middlewares/read');
 const deleteMiddleware = require('./middlewares/delete');
 const deploy = require('./deploy');
+const os = require('os');
+const ifaces = os.networkInterfaces();
+
 
 router.get('/admin/home', async (ctx) => {
-    return ctx.showResult(ctx, 'Serverless function admin api homepage!', 200);
+  return ctx.showResult(ctx, 'Serverless function admin api homepage!', 200);
 });
 
 router.get('/admin/function/tree', readFunction.tree);
@@ -31,5 +36,8 @@ router.post('/admin/middleware/create', createMiddleware);
 router.post('/admin/middleware/update', updateMiddleware);
 
 router.get('/admin/process/deploy', deploy.deploy);
+router.get('/current-ip', (ctx) => {
+  return ctx.showResult(ctx, localIpAdress, 200);
+})
 
 module.exports = router;
