c3bb66eb2aadd35505708af41be0994176b6098b
Minh Tien <tiennm@leanwell.co>
2020-06-23 12:10:50 +0700

    update hander error

--- core/middlewares/middle-function.js
+++ core/middlewares/middle-function.js
@@ -3,14 +3,14 @@
 
 module.exports.handers = async (ctx, next) => {
   ctx.showResult = (ctx, data, status) => {
-    ctx.status = status;
+    ctx.status = status || 200;
     ctx.body = {
       success: true,
       data: data
     }
   };
   ctx.showError = (ctx, message, status) => {
-    ctx.status = status;
+    ctx.status = status || 500;
     ctx.body = {
       success: false,
       message: message
