e2e17913474c3d85cbd053dc07506de784010e3a
Minh Tien <tiennm@leanwell.co>
2020-06-23 16:07:38 +0700

    update default

--- admin-controllers/functions/create.js
+++ admin-controllers/functions/create.js
@@ -22,7 +22,7 @@ module.exports = async (ctx) => {
     }
   }
 
-  await fs.writeFileSync(`${funcPath}/${name}.js`, script || `const moment = require('moment');\nconst express = require('express');\n\nmodule.exports.handlers = async(ctx)=>{\n  return ctx.showResult(ctx,'API function: ${name}', 200); \n};`);
+  await fs.writeFileSync(`${funcPath}/${name}.js`, script || `const moment = require('moment');\n\nmodule.exports.handlers = async(ctx)=>{\n\n  return ctx.showResult(ctx,'API function: ${name}', 200); \n};`);
 
   if (process.env.FAST_DEPLOY === 'true') {
     const result = await deployChildProcess();
